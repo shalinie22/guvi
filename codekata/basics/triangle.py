37a235602079748079e35f8d695b0ac6c715a8b7
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-16 18:50:05 +0530

    Update triangle.py

--- codekata/basics/triangle.py
+++ codekata/basics/triangle.py
@@ -2,10 +2,10 @@
 #function to find the triangle
 def tri(a,b,c):
  #condition to check the sides form triangle
-    if(a+b>=c) or (a+c>=b) or (b+c>=a):
-    	return "yes"
+    if(a+b<=c) or (a+c<=b) or (b+c<=a):
+    	return "no"
     else:
-      	return "no"
+     return "yes"
 #get the sides of the triangle from the user
 x,y,z=map(int,input().split())
 print(tri(x,y,z))
