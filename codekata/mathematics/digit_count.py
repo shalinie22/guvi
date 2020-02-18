1e7fd8b3044f4d77ad4881c4aa417f213e9bb1cf
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-18 14:28:39 +0530

    Create digit_count.py

---
+++ codekata/mathematics/digit_count.py
@@ -0,0 +1,14 @@
+#Count the number of digits of a given number N
+#function to find the number of digits
+def dig(n)
+#initialize c to zero
+    c=0
+#loop to count the digits
+    while n!=0:
+        r=n%10
+        c+=1
+        n=n//10
+    return c
+#get the number from the user
+n=int(input())
+print(dig(n))
