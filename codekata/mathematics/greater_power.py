33608c61e93c403ed855dd3999ef9ae7331a41f4
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-18 14:55:26 +0530

    Create greater_power.py

---
+++ codekata/mathematics/greater_power.py
@@ -0,0 +1,8 @@
+#find its next immediate greater power of 2.
+#function to find the next greater power of 2
+def greater(n):
+#returns the value of n multiplied by 2 
+    return n*2
+#get the number from the user
+n=int(input())
+print(greater(n))
