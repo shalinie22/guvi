120f8226d8ac5f9f2e370506ded1be3b5f6d79c6
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-20 14:38:46 +0530

    Create beautifull.py

---
+++ codekata/strings/beautifull.py
@@ -0,0 +1,12 @@
+#Print ‘1’ when string is beautiful and ‘0’ when it is not
+#function declaration of bes
+def bes():
+#get the input from the user
+    a=input()
+#condition to check the string has 3 characters
+    if(len(a)==3):
+        print("1")
+    else:
+    	print("0")
+#call the function bes
+bes()
