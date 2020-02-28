fd9052b6c7f5dd29471dbbe4726948450414e7cd
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-28 23:53:10 +0530

    Create sum_of_odd_num.js

---
+++ java_script/looping/sum_of_odd_num.js
@@ -0,0 +1,15 @@
+//to print the sum of odd numbers
+var n=prompt("enter the number");
+undefined
+var i=1,s=0;
+undefined
+while(i<=n)
+{
+    if(i%2==1)
+    {
+        s+=i;
+    }
+    i+=1;
+}
+11
+console.log(s);
