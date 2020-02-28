5814ffabc625e1384f65034e372de08a9034d2cc
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-28 23:53:59 +0530

    Update sum_of_odd_num.js

--- java_script/looping/sum_of_odd_num.js
+++ java_script/looping/sum_of_odd_num.js
@@ -1,8 +1,6 @@
 //to print the sum of odd numbers
 var n=prompt("enter the number");
-undefined
 var i=1,s=0;
-undefined
 while(i<=n)
 {
     if(i%2==1)
@@ -11,5 +9,4 @@ while(i<=n)
     }
     i+=1;
 }
-11
 console.log(s);
