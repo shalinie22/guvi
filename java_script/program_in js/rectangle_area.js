9e3d772a7c6c5aebe8ddf342bf5c1f10eadf908a
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-02-28 11:38:35 +0530

    Create rectangle_area.js

---
+++ java_script/program_in js/rectangle_area.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const at = [];
+
+inp.on("line", (data) => {
+	at.push(data);
+});
+
+inp.on("close", () => {
+	var a=at[0],b=at[1];
+  	var c=parseInt(a)*parseInt(b);
+	console.log(parseInt(c));
+});
