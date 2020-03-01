6e14f16881ecfdfaf3e025d4322919620dd5e2ba
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-02 00:57:25 +0530

    Update 1st_3_multiple.js

--- java_script/program_in js/1st_3_multiple.js
+++ java_script/program_in js/1st_3_multiple.js
@@ -1,19 +1,22 @@
 const readline=require('readline');
-const inp = readline.createInterface({
-  input:process.stdin
-});
-inp.on("line",(n)=>{
-  if(n!=0)
-  {
-  	var s=' ';
-  	for(var i=1;i<=n;i++)
-  	{
-    	s+=(9*i)+' ';
-  	}
-  	console.log(s);
-  }
-  else
-  {
-    console.log("NULL");
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0],s='';
+  if(a==0){
+    console.log("NULL")}
+  else{
+    for(var i=1;i<a+1;i++){
+    if(i<a){
+    s+=9*i+" "
+    }
+      else{
+      s+=9*i
+      break}
+    }
+    console.log(s)
   }
 });
