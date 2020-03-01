5f5943a0c901e809841d78423f76165bd3de9748
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-02 00:56:38 +0530

    Update n_mutiple.js

--- java_script/program_in js/n_mutiple.js
+++ java_script/program_in js/n_mutiple.js
@@ -1,12 +1,10 @@
 const readline=require('readline');
-const inp = readline.createInterface({
-  input:process.stdin
-});
-inp.on("line",(n)=>{
-  	var s=' ';
-  	for(var i=1;i<=3;i++)
-  	{
-    	s+=(n*i)+'';
-  	}
-  	console.log(s);
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=parseInt(n[0].split(" "));
+  console.log(n[0]+" "+(2*n[0])+" "+(3*n[0]));
 });
