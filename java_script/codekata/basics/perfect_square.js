b36e07f43345f07e400783dd77c23223b6da67c2
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-01 14:04:56 +0530

    Create perfect_square.js

---
+++ java_script/codekata/basics/perfect_square.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const a = [];
+
+inp.on("line", (data) => {
+	a.push(data);
+});
+
+inp.on("close", () => {
+    var s=a[0].split(" ");
+    var f=parseInt(s[0]);
+    var d=parseInt(s[1]);
+    var p=f*d;
+    if(p==(f*f))
+    {
+        console.log("yes");
+    }
+    else
+    {
+        console.log("no");
+    }
+});
