722296206a245ef9c87f76c2508328959cf11aff
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-06 00:11:23 +0530

    Create roll.js

---
+++ java_script/roll_over/roll.js
@@ -0,0 +1,11 @@
+function Image1(){
+    let x=["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg","pic6.jpg"]
+    let y = x[Math.floor(Math.random() * x.length)];
+    document.getElementById("pic").src =y;
+  }
+  
+  function Image2(){
+    let x=["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg","pic6.jpg"]
+    let y = x[Math.floor(Math.random() * x.length)];
+    document.getElementById("pic").src =y;
+  }
