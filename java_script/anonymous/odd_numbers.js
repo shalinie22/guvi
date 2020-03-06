27fc2dcc3d7d720a114479b6fb50a30765ba6d9d
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-06 23:19:16 +0530

    Update odd_numbers.js

--- java_script/anonymous/odd_numbers.js
+++ java_script/anonymous/odd_numbers.js
@@ -1,5 +1,12 @@
 //to print the odd numbers 
 var a=[1,2,3,4,5,6];
-var odd=a.filter(function(b){
-    return b%2!=0;});
-console.log(odd);
+var s=function(a){
+    for(var i=0;i<a.length;i++)
+    {
+        if(a[i]%2!=0)
+        {
+           console.log(a[i]);
+        }
+    }
+}
+s(a);
