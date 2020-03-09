3a97f6dbc7b49a7e67dc1dc08c4a157c214d4f30
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-09 22:25:11 +0530

    Create min_element.js

---
+++ java_script/jsprograms/min_element.js
@@ -0,0 +1,20 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);
+});
+inp.on("close",()=>{
+    var e=a[0].split(" ");
+    var t=parseInt(e[0]);
+    for(var i=0;i<e.length;i++)
+    {
+        if(t>parseInt(e[i]))
+        {
+            t=e[i];
+        }
+    }
+    console.log(t);
+});
