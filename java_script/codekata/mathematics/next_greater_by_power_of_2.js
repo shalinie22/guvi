9c04318ee3b91a88f3f859f07fb67d6a06012606
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-03-10 23:26:51 +0530

    Create next_greater_by_power_of_2.js

---
+++ java_script/codekata/mathematics/next_greater_by_power_of_2.js
@@ -0,0 +1,15 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+const a=[];
+inp.on("line",(data)=>{
+    a.push(data);
+});
+inp.on("close",()=>{
+    var d=parseInt(a[0]);
+    var b=a[0].split("");
+    var p=d*2;
+    console.log(p);
+        
+});
