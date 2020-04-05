2c2b0dfbfe18f7ff0c55b08c3b5f73fa0496fcda
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-04-05 22:08:50 +0530

    Create great_no.js

---
+++ java_script/codekata/numbers/great_no.js
@@ -0,0 +1,27 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+const a=[];
+inp.on("line",(data)=>{
+    a.push(data);
+});
+inp.on("close",()=>{
+    var d=parseInt(a[0]);
+    var b=a[0].split("");
+    var n=1;
+    var s=0;
+    for(var i=0;i<b.length;i++)
+    {
+        n*=parseInt(b[i]);
+        s+=parseInt(b[i]);
+    }
+    if((s+n)==d)
+    {
+      console.log("Great");
+    }
+    else
+    {
+        console.log("no");
+    }
+});
