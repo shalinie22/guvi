b64267c89d8bbada9b35187ce70d6369b90b76f0
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-04-05 14:07:53 +0530

    Update reverse_hello_world.js

--- java_script/codekata/basics/reverse_hello_world.js
+++ java_script/codekata/basics/reverse_hello_world.js
@@ -1,30 +1,12 @@
 //to reverse the given words 
-const readline = require('readline');
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const a = [];
-
-inp.on("line", (data) => {
-	a.push(data);
-});
-
-inp.on("close", () => {
-    var s=a[0].split(" ");
-    var r='';
-    for(i=(s.length)-1;i>=0;i--)
-    {
-      if(i<s.length)
-      {
-        r+=s[i]+' ';
-      }
-      else
-      {
-        r+=s[i]+'';
-        break
-      }
-    }
-    console.log(r);   
-});
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var b=[];
+  b=a[0].split(" ");
+  console.log(b[1],b[0]);
+  });
