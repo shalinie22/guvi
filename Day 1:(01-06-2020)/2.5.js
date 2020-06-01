3414019881d2a0a02eb58b4875e5ddb9e34c841d
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-06-01 21:07:36 +0530

    Create 2.5.js

---
+++ Day 1:(01-06-2020)/2.5.js
@@ -0,0 +1,36 @@
+//2.5 Find the datatype of author age in Nodejs Object.
+//2.3 Insert a new reader in the readers array for Nodejs.
+var library=[{
+                "title":"javascript",
+                "price": 500,
+                "readers":[{
+                            "name":"person1",
+                            "count":300
+                            },
+                            {
+                            "name":"person2",
+                            "count":400
+                            }],
+                "authordetails":{
+                                "name":"raj",
+                                "age":40
+                                }
+            },
+            {
+                 "title":"nodejs",
+                "price": 600,
+                "readers":[],
+                 "authordetails":{
+                                "name":"raj",
+                                "age":40
+                                }
+                
+            }];
+    // function to find the type of the authordetails's age for nodejs 
+    function type_of_age(a){
+        // to print the type of the authordetails's age for nodejs
+    console.log(typeof(library[1].authordetails.age));
+    }
+    //call the function type_of_age by passing the parameter library
+    type_of_age(library);
+    
