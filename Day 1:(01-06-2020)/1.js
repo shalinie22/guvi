f9f35d907c1430c6363ba16b725c9d6e22457500
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-06-01 22:27:33 +0530

    Create 1.js

---
+++ Day 1:(01-06-2020)/1.js
@@ -0,0 +1,55 @@
+/*1. Convert specifications given below in image to Javascript Object;
+Link :
+https://www.mohanbn.com/wp-content/uploads/2008/11/a-star-tech-specs.jpg*/
+var technical_specifications=[
+                                "dimensions":{
+                                               "overall_length(mm)":3,500,
+                                               "overall_width(mm)":1,600,
+                                               "overall_height(mm)":1,490,
+                                               "wheelbase(mm)":2,360,
+                                               "track_width":{
+                                                                "front(mm)":1,405,
+                                                                "rear(mm)":1,400
+                                                            },
+                                               "minimum_turning_radius(m)":4.5,
+                                               "minimum_ground_clearance(mm)":170
+                                            },
+                                            
+                                "capacities":{
+                                                "sealing_capacity(persons)":5,
+                                                "fuel_tamk_capacity(litres)":35
+                                            },
+                                            
+                                "engine":{
+                                                "type":"KB-series",
+                                                "Number_of_cylinders":3,
+                                                "Number_of_valves":12,
+                                                "Capacity(cc/cm^3)":998,
+                                                "Bore_X_stroke(mm)":"73.0*79.5",
+                                                "Compression_ratio":10:1,
+                                                "Maximum_power(Ps/rpm)":"67/6,200",
+                                                "Maximum_torque(Nm/rpm)":"90/3,500",
+                                                "fuel_distribution":"multipoint_injection"
+                                                
+                                         },
+                                "transmission":{
+                                                "type":"5-speed_Mt"
+                                                },   
+                                "Chasis":{
+                                            "steering":"rack&pinion,power assisted",
+                                            "brakes":{
+                                                                "front":"ventilated_dics",
+                                                                "rear":"Drums"
+                                                      },
+                                            "suspension":{
+                                                                "front":"MacPherson_strut& coil spring",
+                                                                "rear":"Isolated trailing link & coil spring"
+                                                      },
+                                            "Shock absorbers":"gas_filled",
+                                            "tyre(tubeless)":"155/80R13"
+                                        },
+                                "weights":{
+                                            "Kerb weight(min, with full options)(kg)":"860-880",
+                                            "Gross vehicle weight(kg)":1,320
+                                    
+                                }]
