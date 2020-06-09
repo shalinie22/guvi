096b08486d0b843054c42b81a87bbd8393a6c08c
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-06-09 10:06:25 +0530

    Update grid.js

--- reactjs/grid.js
+++ reactjs/grid.js
@@ -1,51 +1,43 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>React Session</title>
-    <meta charset="UTF-8" />
-
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    />
-
     <script
-      crossorigin
-      src="https://unpkg.com/react@16/umd/react.development.js"
-    ></script>
+    crossorigin
+    src="https://unpkg.com/react@16/umd/react.development.js"
+  ></script>
 
-    <script
-      crossorigin
-      src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"
-    ></script>
-  </head>
-  
-  <body>
-    <div id="body"></div>
+  <script
+    crossorigin
+    src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"
+  ></script>
+</head>
+<body>
+    <div id="div1"></div>
+</body>
+<script>
+    const div1=document.getElementById("div1")
+    const subdiv = 
+       React.createElement("div", {  style:{border:"2px solid black",width:"150px",height:"100px"} });
 
-    <script>
-      const rootelement = document.getElementById("body");
-      const element=React.createElement(
-          "div",
-          {className:"grid-item",style:{border:"2px solid black",backgroundColor: "white",border: "black",height:"100px",width:"200px"}}     );
-      const divelement=React.createElement(
-          "div",
-        {className:"grid-container",style:{display:"grid",height:"100px",width:"100px",gridGap:"4px",gridTemplateColumns: "auto auto auto auto",backgroundColor: "gray"}},
-          
-          element,
-          element,
-          element,
-          element,
-          element,
-          element,
-          element,
+      const maindiv = React.createElement(
+       "div",
+  {style:{width:"500px",height:"300px",display: "grid",
+  gridTemplateColumns: "auto auto auto auto",gridGap: "5px"}},
+      subdiv,
+        subdiv,
+        subdiv,
+       subdiv,
+       subdiv,
+        subdiv,
+        subdiv,
+       subdiv,
+       subdiv,
+        subdiv,
+        subdiv,
+       subdiv
+       );
 
-      
-      );
-      ReactDOM.render(divelement,rootelement);
-      
+       ReactDOM.render(maindiv, div1);
 
     </script>
-  </body>
-</html>
-
+    </html>
