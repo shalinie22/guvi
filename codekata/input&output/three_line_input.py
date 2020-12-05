46a15d1f1573e77a4daf459c44949b48f55d49d8
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-12-05 12:29:40 +0530

    Create three_line_input.py

---
+++ codekata/input&output/three_line_input.py
@@ -0,0 +1,28 @@
+
+"""
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+First-line indicates two integers separated by space. Second-line indicates two integers separated by space. Third-line indicates two integers separated by space.
+
+Output Description:
+Print the input in the same format.
+
+Sample Input :
+2 4
+2 4
+2 4
+
+Sample Output :
+2 4
+2 4
+2 4
+
+"""
+
+x,y=list(map(int,input("").split()))
+print(x,y)
+s,t=list(map(int,input("").split()))
+print(s,t)
+u,v=list(map(int,input("").split()))
+print(u,v)
