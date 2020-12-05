64c9a47faa7d0c0a085906723fb542ea0a7db176
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-12-05 13:01:16 +0530

    Create input&outputthree integers separated by space.py

---
+++ codekata/input&outputthree integers separated by space.py
@@ -0,0 +1,27 @@
+
+"""
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+First-line indicates two integers separated by space. Second-line indicates three integers separated by space. Third-line indicates three integers separated by space
+
+Output Description:
+Print the input in the same format.
+
+Sample Input :
+2 5
+2 5 6
+2 4 5
+
+Sample Output :
+2 5
+2 5 6
+2 4 5
+"""
+
+x,y=list(map(int,input("").split()))
+print(x,y)
+r,s,t=list(map(int,input("").split()))
+print(r,s,t)
+a,b,c=list(map(int,input("").split()))
+print(a,b,c)
