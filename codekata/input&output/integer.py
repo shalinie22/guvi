70f51fbd70671bb64dec473a5d26d69157b64ac4
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-12-05 11:56:15 +0530

    Create integer.py

---
+++ codekata/input&output/integer.py
@@ -0,0 +1,18 @@
+"""
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+To take an integer value
+
+Output Description:
+Print the integer value
+
+Sample Input :
+2
+
+Sample Output :
+2
+
+"""
+a=int(input())
+print(a)
